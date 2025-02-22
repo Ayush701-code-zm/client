ff811d55cdc5cd24a87567f559882646a657d4f3
Ayush701-code-zm <h3yayush@gmail.com>
2025-02-23 04:28:50 +0530

    okkkkk

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the mode (e.g., production, development)
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    define: {
+      "process.env": env, // Make sure env variables are available
+    },
+  };
 });
